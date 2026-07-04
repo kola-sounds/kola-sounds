aa0a42d1b67d1a7d69c02d04d4ed525dc79a336a
kola-sounds <kolasoundsofgrace@gmail.com>
2026-07-04 11:20:02 +0300

    Add About section to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
+import About from "../components/About";
 
 export default function Home() {
   return (
       <main className="min-h-screen bg-black text-white">
             <Navbar />
                   <Hero />
-                      </main>
-                        );
-                        }
+                        <About />
+                            </main>
+                              );
+                              }
