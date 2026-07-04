981090cacf210696857110ec7038d3818d4aec80
kola-sounds <kolasoundsofgrace@gmail.com>
2026-07-04 13:50:46 +0300

    update page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import About from "../components/About";
+import Music from "../components/Music";
 
 export default function Home() {
   return (
       <main className="min-h-screen bg-black text-white">
-            <Navbar />
-                  <Hero />
-                        <About />
-                            </main>
+              <Navbar />
+                <Hero />
+                  <About />
+                    <Music />
+                    </main>
                               );
                               }
